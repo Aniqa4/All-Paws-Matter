35dc191834d2203a585ee1d63cba6e3992cbc2e8
Aniqa <aanika.chowdhury04@gmail.com>
2023-09-09 06:51:49 +0600

    added more nested routes

--- app/Sidebar/Sidebar.tsx
+++ app/Sidebar/Sidebar.tsx
@@ -6,8 +6,12 @@ export default function Sidebar() {
     <div>
         <ul className='w-60 shadow border-e h-screen px-10 py-5 text-gray-700 font-semibold'>
             <li className='border-b py-3'><Link href={'/HomePage'}>Home</Link></li>
-            <li className='border-b py-3'><Link href={'/HomePage/vet-points'}>Vet Points</Link></li>
-            <li className='border-b py-3'><Link href={'/HomePage/rescue'}>Rescue</Link></li>
+            <li className='border-b py-3'><Link href={'/HomePage/animal-rescue'}>Animal Rescue</Link></li>
+            <li className='border-b py-3'><Link href={'/HomePage/veterinary-care'}>Veterinary Care</Link></li>
+            <li className='border-b py-3'><Link href={'/HomePage/emergency-response'}>Emergency Response</Link></li>
+            <li className='border-b py-3'><Link href={'/HomePage/adoption-programs'}>Adoption Programs</Link></li>
+            <li className='border-b py-3'><Link href={'/HomePage/education-and-Outreach'}>Education & Outreach</Link></li>
+            <li className='border-b py-3'><Link href={'/HomePage/advocacy-and-lobbying'}>Advocacy and Lobbying</Link></li>
         </ul>
     </div>
   )
